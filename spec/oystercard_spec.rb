0bfa96316bfc75eb2e2c4390fe57b269bf9afd44
Adam Erdem Er <adamerdemer@gmail.com>
2017-04-19 04:57:45 +0100

    Added in maximum balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,10 +7,16 @@ describe Oystercard do
     expect(oystercard.balance).to eq(0)
   end
 
+  it "raises an error if the maximum balance is exceeded" do
+    maximum_balance = Oystercard::MAXIMUM_BALANCE
+    oystercard.top_up(maximum_balance)
+    expect{ oystercard.top_up 1 }.to raise_error "Maximum balance exceeded"
+  end
+
   describe "#top_up" do
     it { is_expected.to respond_to(:top_up).with(1).argument }
 
-    it 'can top up the balance' do
+    it "can top up the balance" do
       expect{ oystercard.top_up 1 }.to change{ oystercard.balance }.by 1
     end
 
