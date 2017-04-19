cb2ec078b718411df9b57267413c8a971678ab38
Adam Erdem Er <adamerdemer@gmail.com>
2017-04-19 04:42:32 +0100

    Added top_up method and test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,15 @@ describe Oystercard do
   it "has a balanace of zero" do
     expect(oystercard.balance).to eq(0)
   end
+
+  describe "#top_up" do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'can top up the balance' do
+      expect{ oystercard.top_up 1 }.to change{ oystercard.balance }.by 1
+    end
+
+  end
+
+
 end
