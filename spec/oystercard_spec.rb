22b2274ddb49f533b91b955c3e3cfb6d20776008
Adam Erdem Er <adamerdemer@gmail.com>
2017-04-21 17:52:52 +0100

    Refactored rspec tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,21 +7,11 @@ describe Oystercard do
 
   let(:station){ double :station}
 
-  it "has a balanace of zero" do
-    expect(oystercard.balance).to eq(0)
-  end
-
   it 'is initially not in a journey' do
     expect(oystercard.in_journey?).to be false
   end
 
   describe "#touch_in" do
-
-    it "can touch in" do
-      oystercard.top_up(1)
-      expect(oystercard.touch_in(@station)).to be nil
-    end
-
 
     it "raises an error when minimum fare not reached" do
       expect{ oystercard.touch_in(@station) }.to raise_error "insufficent balance"
@@ -50,27 +40,28 @@ describe Oystercard do
   end
 
   describe "#touch_out" do
-    it "can touch out" do
-      oystercard.top_up minimum_fare
-      oystercard.touch_in(@station)
-      expect(oystercard.touch_out(@station)).to be nil
-    end
 
     it "charges the card on touch_out" do
       expect{oystercard.touch_out(@station)}.to change{oystercard.balance}.by(-minimum_fare)
     end
   end
 
-  it "raises an error if the maximum balance is exceeded" do
-    maximum_balance = Oystercard::MAXIMUM_BALANCE
-    oystercard.top_up maximum_balance
-    expect{ oystercard.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
-  end
+  describe "#balance" do
 
-  describe "#top_up" do
+    it "has a balanace of zero" do
+      expect(oystercard.balance).to eq(0)
+    end
+
+    it "raises an error if the maximum balance is exceeded" do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      oystercard.top_up maximum_balance
+      expect{ oystercard.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
+    end
+
     it "can top up the balance" do
       expect{ oystercard.top_up 1 }.to change{ oystercard.balance }.by 1
     end
   end
+
 
 end
